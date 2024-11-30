5da7a2bdd6520618d8dd129a9df5098b56f39705
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-11-30 09:57:41 -0800

    chore: refactored Util.cs

--- XLAuthenticatorNet/Support/Util.cs
+++ XLAuthenticatorNet/Support/Util.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using XLAuthenticatorNet.Extensions;
 using XLAuthenticatorNet.Windows;
 
-namespace XLAuthenticatorNet.Domain;
+namespace XLAuthenticatorNet.Support;
 
 /// <summary>
 /// The util class
